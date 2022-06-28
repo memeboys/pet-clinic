9fd3caf4940401e2b38b17386a7a6114975bef28
dron11231 <astar.dron@gmail.com>
2022-06-28 16:06:32 +0300

    transfers the auth-form component to the pages folder

---
+++ src/components/auth-form/AuthForm.tsx
@@ -0,0 +1,69 @@
+import { Formik, Form, Field } from 'formik';
+import React from 'react';
+import * as yup from 'yup';
+import './AuthForm.scss';
+
+const AuthForm: React.FC = () => {
+  const validationsSchema = yup.object().shape({
+    email: yup.string().email('Incorrect email').required('Email is required'),
+    password: yup.string().required('Password is required'),
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onSubmit = (data: { email: string, password: string }) => {};
+
+  return (
+    <div className="container">
+      <Formik initialValues={{ email: '', password: '' }} onSubmit={onSubmit} validationSchema={validationsSchema}>
+        {({
+          handleChange, handleBlur, values, isValid, dirty, touched, errors,
+        }) => (
+          <Form className="auth-form">
+            <div className="inner-wrapper">
+              <h2 className="auth-form__title">Sign-in</h2>
+              <div className="input-wrapper">
+                <label htmlFor="email">
+                  Email
+                  <Field
+                    id="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                  />
+                  {touched.email && errors.email && <span className="input-wrapper__error">{errors.email}</span>}
+                </label>
+              </div>
+              <div className="input-wrapper">
+                <label htmlFor="password">
+                  Password
+                  <Field
+                    placeholder="Password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    id="password"
+                    type="password"
+                    name="password"
+                  />
+                  {touched.password && errors.password && <span className="input-wrapper__error">{errors.password}</span>}
+                </label>
+              </div>
+              <button
+                className="auth-form__btn"
+                disabled={!isValid && !dirty}
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default AuthForm;
--- src/pages/auth-page/AuthPage.tsx
+++ src/pages/auth-page/AuthPage.tsx
@@ -1,69 +1,8 @@
-import { Formik, Form, Field } from 'formik';
 import React from 'react';
-import * as yup from 'yup';
-import './AuthPage.scss';
+import AuthForm from '../../components/auth-form/AuthForm';
 
-function AuthPage () {
-  const validationsSchema = yup.object().shape({
-    email: yup.string().email('Incorrect email').required('Email is required'),
-    password: yup.string().required('Password is required'),
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const onSubmit = (data: { email: string, password: string }) => {};
-
-  return (
-    <div className="container">
-      <Formik initialValues={{ email: '', password: '' }} onSubmit={onSubmit} validationSchema={validationsSchema}>
-        {({
-          handleChange, handleBlur, values, isValid, dirty, touched, errors,
-        }) => (
-          <Form className="auth-form">
-            <div className="inner-wrapper">
-              <h2 className="auth-form__title">Sign-in</h2>
-              <div className="input-wrapper">
-                <label htmlFor="email">
-                  Email
-                  <Field
-                    id="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    type="text"
-                    name="email"
-                    placeholder="Email"
-                  />
-                  {touched.email && errors.email && <span className="input-wrapper__error">{errors.email}</span>}
-                </label>
-              </div>
-              <div className="input-wrapper">
-                <label htmlFor="password">
-                  Password
-                  <Field
-                    placeholder="Password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    id="password"
-                    type="password"
-                    name="password"
-                  />
-                  {touched.password && errors.password && <span className="input-wrapper__error">{errors.password}</span>}
-                </label>
-              </div>
-              <button
-                className="auth-form__btn"
-                disabled={!isValid && !dirty}
-                type="submit"
-              >
-                Submit
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+function AuthPage ():React.ReactElement {
+  return <AuthForm />;
 }
 
 export default AuthPage;
