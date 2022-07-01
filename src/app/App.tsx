0b6c51c8811d5e9f75d9e5b67ec59938598476ea
dron11231 <astar.dron@gmail.com>
2022-07-01 18:30:06 +0300

    Добавляет футер

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPage from '../pages/auth-page/AuthPage';
 import MainPage from '../pages/main-page/MainPage';
+import Footer from '../components/footer/Footer';
 import 'antd/dist/antd.css';
 
 function App () {
   return (
     <Router>
       <h2>Header</h2>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/auth-page" element={<AuthPage />} />
-      </Routes>
-      <h2>Footer</h2>
+      <div className="main-content-wrapper">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/auth-page" element={<AuthPage />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
