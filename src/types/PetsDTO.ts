81e247dad31781a3658e7f79ead8a5d7e51ece43
dron11231 <astar.dron@gmail.com>
2022-07-04 14:57:04 +0300

    Добавляет enum PetType

--- src/types/PetsDTO.ts
+++ src/types/PetsDTO.ts
@@ -1,9 +1,13 @@
+enum PetType {
+  CAT = 'CAT',
+  DOG = 'DOG',
+}
+
 export interface PetDTO {
   id: number,
   name: string,
   avatar: string,
   birthDay: string,
   notificationCount: number,
-  petType: string
-
+  petType: PetType
 }
