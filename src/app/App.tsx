f952df65b80340a4a1303f41d505dd207f5eb2f2
Алексей Рябинцев <nightout2017@gmail.com>
2022-07-04 09:49:00 +0000

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,19 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPage from '../pages/auth-page/AuthPage';
 
-const App:FC = () => (
-  <Router>
-    <h2>Header</h2>
-    <Routes>
-      <Route path="/" element={<h2>Main</h2>} />
-      <Route path="/auth-page" element={<AuthPage />} />
-    </Routes>
-    <h2>Footer</h2>
-  </Router>
-);
+function App () {
+  return (
+    <Router>
+      <h2>Header</h2>
+      <Routes>
+        <Route path="/" element={<h2>Main</h2>} />
+        <Route path="/auth-page" element={<AuthPage />} />
+      </Routes>
+      <h2>Footer</h2>
+    </Router>
+  );
+}
+
 
 export default App;
