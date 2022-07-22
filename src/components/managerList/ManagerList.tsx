8577872374602b9a359bc9ceea3578970cc05e5c
Tosha-666 <qwerty21042018@mail.ru>
2022-07-22 16:34:08 +0300

    add styles

--- src/components/managerList/ManagerList.tsx
+++ src/components/managerList/ManagerList.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 import classes from './ManagerList.module.scss';
 
 const Manager:React.FC = () => (
-  <div className={classes.wrapper}>
+  <aside className={classes.navbar}>
     <ul className={classes.menu}>
-      <li className={classes.menu__item}><Link to="/manager" className={classes.link}>Категории</Link></li>
-      <li className={classes.menu__item}><Link to="news" className={classes.link}>Новости</Link></li>
-      <li className={classes.menu__item}><Link to="medicine" className={classes.link}>Лекарства</Link></li>
+      <li><NavLink to="/manager" className={classes.link}>Категории</NavLink></li>
+      <li><NavLink to="news" className={classes.link}>Новости</NavLink></li>
+      <li><NavLink to="medicine" className={classes.link}>Лекарства</NavLink></li>
     </ul>
     <main className={classes.content}><Outlet /></main>
 
-  </div>
+  </aside>
 
 );
 
