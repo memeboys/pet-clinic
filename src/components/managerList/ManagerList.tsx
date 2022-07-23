fb3d8876d5da50710fedfd321a6710ea726c5c6f
Tosha-666 <qwerty21042018@mail.ru>
2022-07-23 17:27:02 +0300

    SP-21 try to do active navlink

--- src/components/managerList/ManagerList.tsx
+++ src/components/managerList/ManagerList.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import classes from './ManagerList.module.scss';
 
-const Manager:React.FC = () => (
-  <aside className={classes.navbar}>
-    <ul className={classes.menu}>
-      <li><NavLink to="/manager" className={classes.link}>Категории</NavLink></li>
-      <li><NavLink to="news" className={classes.link}>Новости</NavLink></li>
-      <li><NavLink to="medicine" className={classes.link}>Лекарства</NavLink></li>
-    </ul>
-    <main className={classes.content}><Outlet /></main>
+const Manager:React.FC = () =>
 
-  </aside>
+// const activeStyle = ( isActive ) =>  isActive ? {classes.active} : ''
 
-);
+  (
+    <aside className={classes.navbar}>
+      <ul className={classes.menu}>
+        <li><NavLink to="/" className={classes.link}> Категории</NavLink></li>
+        <li><NavLink to="news" className={classes.link}>Новости</NavLink></li>
+        <li><NavLink to="medicine" className={classes.link}>Лекарства</NavLink></li>
+        {/* <li><NavLink to="news" className="link">Лекарства</NavLink></li>
+      <li><NavLink to="medicine" className="link">Лекарства</NavLink></li>
+      <li><NavLink to="news" className="link">Лекарства</NavLink></li>
+      <li><NavLink to="medicine" className="link">Лекарства</NavLink></li> */}
+      </ul>
+      <main className="content"><Outlet /></main>
 
+    </aside>
+  );
 export default Manager;
