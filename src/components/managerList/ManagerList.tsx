91815e76e9999ee510a732a0ed803df1dd602d0c
Tosha-666 <qwerty21042018@mail.ru>
2022-07-28 18:19:10 +0300

    add clsx, fix classes

--- src/components/managerList/ManagerList.tsx
+++ src/components/managerList/ManagerList.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
+import clsx from 'clsx';
 import { Outlet, NavLink } from 'react-router-dom';
 import classes from './ManagerList.module.scss';
 
-const Manager:React.FC = () => {
-  const activeClass = ({ isActive }:{ isActive:boolean }) => (
+const Manager:React.FC = () => (
+  <aside className={classes.navbar}>
+    <ul className={classes.menu}>
+      <li>
+        <NavLink
+          to="categories"
+          className={({ isActive }) => clsx(classes.link, { [classes.active]: isActive })}
+        >
+          Категории
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="news"
+          className={({ isActive }) => clsx(classes.link, { [classes.active]: isActive })}
+        >
+          Новости
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="medicine"
+          className={({ isActive }) => clsx(classes.link, { [classes.active]: isActive })}
+        >
+          Лекарства
+        </NavLink>
+      </li>
+    </ul>
+    <main className={classes.content}><Outlet /></main>
 
-    isActive
-      ? `${classes.link} ${classes.active}`
-      : `${classes.link}`
-  );
-
-  return (
-    <aside className={classes.navbar}>
-      <ul className={classes.menu}>
-        <li>
-          <NavLink to="categories" className={activeClass}>Категории</NavLink>
-        </li>
-        <li>
-          <NavLink to="news" className={activeClass}>Новости</NavLink>
-        </li>
-        <li>
-          <NavLink to="medicine" className={activeClass}>Лекарства</NavLink>
-        </li>
-      </ul>
-      <main className={classes.content}><Outlet /></main>
-
-    </aside>
-  );
-};
+  </aside>
+);
 
 export default Manager;
