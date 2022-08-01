712979d74e3c1a8374e5a00ef04185daf78ffb82
Tosha-666 <qwerty21042018@mail.ru>
2022-08-01 18:33:34 +0300

    SP-21 fix styles delete request

--- src/components/managerList/ManagerList.tsx
+++ src/components/managerList/ManagerList.tsx
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import { Outlet, NavLink } from 'react-router-dom';
 import classes from './ManagerList.module.scss';
 
 const Manager:React.FC = () => {
-  const [categories, setCategories] = useState<string[]>([]);
-
-  useEffect(() => {
-    setTimeout(() => setCategories(['News', 'Medicine']), 500);
-  }, []);
-
   const activeClass = ({ isActive }:{ isActive:boolean }) => (clsx(classes.link, { [classes.active]: isActive }));
 
   return (
@@ -17,17 +11,31 @@ const Manager:React.FC = () => {
       <aside className={classes.navbar}>
         <header className={classes.list_header}>Категории</header>
         <ul className={classes.menu_list}>
-          {categories.map((category) => (
-            <li key={category}>
-              <NavLink
-                to={category}
-                className={activeClass}
-              >
-                {category}
-              </NavLink>
-            </li>
-          ))}
 
+          <li>
+            <NavLink
+              to="news"
+              className={activeClass}
+            >
+              Новости
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="medicines"
+              className={activeClass}
+            >
+              Лекарства
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="contacts"
+              className={activeClass}
+            >
+              Контакты
+            </NavLink>
+          </li>
         </ul>
       </aside>
       <main className={classes.manager_content}><Outlet /></main>
