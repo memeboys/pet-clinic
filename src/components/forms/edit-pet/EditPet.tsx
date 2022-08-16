300362ff8ae6b5946d98568e7045ef42a4006afa
memeboys <simonsnkn@gmail.com>
2022-08-16 18:52:22 +0300

    SP-33: add-styles-and-validation-form

--- src/components/forms/edit-pet/EditPet.tsx
+++ src/components/forms/edit-pet/EditPet.tsx
@@ -10,44 +10,98 @@ export interface EditPetProps  {
 }
 
 const EditPet: React.FC<EditPetProps> = ({ pet }) => {
+
+  const validationSchema = yup.object().shape({
+    name: yup.string().required('Поле обязательно для заполнения'),
+    weight: yup.string().required('Поле обязательно для заполнения'),
+    breed: yup.string().required('Поле обязательно для заполнения'),
+    color: yup.string().required('Поле обязательно для заполнения'),
+    description: yup.string().required('Поле обязательно для заполнения'),
+    avatar: yup.string().required('Поле обязательно для заполнения'),
+    birthDay: yup.string().required('Поле обязательно для заполнения'),
+  });
+
   return (
-    <Formik
-      initialValues={pet}
-      onSubmit={(values) => console.log(values)}
-    >
-      <Form>
-        <Field name="name" type="text" />
-
-        <Field name="weight" type='text' />
-
-        <Field name="breed" type='text' />
-
-        <Field name="color" type='text' />
-
-        <Field name="description" type='text' />
-
-        <Field name="avatar" type="text" />
-
-        <Field name="birthDay" type="text" />
-
-        <Field name="petType" as="select">
-          <option value={PetType.CAT}>Cat</option>
-          <option value={PetType.DOG}>Dog</option>
-        </Field>
-
-        <Field name="gender" as="select">
-          <option value={PetGender.MALE}>Мужской</option>
-          <option value={PetGender.FEMALE}>Женский</option>
-          <option value={PetGender.OTHER}>Другой</option>
-        </Field>
-
-        <Field name="size" as="select">
-          <option value={PetSize.SMALL}>SMALL</option>
-        </Field>
-
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+    <div className={classes.container}>
+      <Formik
+        initialValues={pet}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <Form className={classes.form}>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='name'>
+              Имя
+              <Field name="name" type="text" />
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='weight'>
+              Вес
+              <Field name="weight" type='text' />
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='breed'>
+              Порода
+              <Field name="breed" type='text' />
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='color'>
+              Окрас
+              <Field name="color" type='text' />
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='description'>
+              Описание
+              <Field name="description" type='text' />
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='avatar'>
+              Фотография
+              <Field name="avatar" type="text" />
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='birthDay'>
+              Дата рождения
+              <Field name="birthDay" type="text" />
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='petType'>
+              Вид
+              <Field name="petType" as="select">
+                <option value={PetType.CAT}>Cat</option>
+                <option value={PetType.DOG}>Dog</option>
+              </Field>
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='gender'>
+              Пол
+              <Field name="gender" as="select">
+                <option value={PetGender.MALE}>Мужской</option>
+                <option value={PetGender.FEMALE}>Женский</option>
+                <option value={PetGender.OTHER}>Другой</option>
+              </Field>
+            </label>
+          </div>
+          <div className={classes.input_wrapper}>
+            <label className={classes.input} htmlFor='size'>
+              Размер
+              <Field name="size" as="select">
+                <option value={PetSize.SMALL}>SMALL</option>
+              </Field>
+            </label>
+          </div>
+          <button type="submit">Отправить</button>
+        </Form>
+      </Formik>
+    </div>
   );
 }
 
