ccdb73f88954447047ceaf3df381e6db6dcd3271
ArtemMayorov <artmai.pg@outlook.com>
2022-08-16 23:34:47 +0300

    add custom arrows

--- src/components/sidebar-info/SidebarBlock.tsx
+++ src/components/sidebar-info/SidebarBlock.tsx
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
 import 'swiper/modules/navigation/navigation.scss';
 import 'swiper/swiper.scss';
-import { Navigation } from 'swiper';
 import classes from './SidebarInfo.module.scss';
 
 type PropTypes = {
   header: string;
   text: string;
 };
+const SidebarBlock: React.FC<PropTypes> = ({ header, text }) => {
+  const navigationPrevRef = useRef<HTMLDivElement>(null);
+  const navigationNextRef = useRef<HTMLDivElement>(null);
 
-const SidebarBlock: React.FC<PropTypes> = ({ header, text }) => (
-  <div className={classes.block}>
-    <header className={classes.header}>{header}</header>
-    <Swiper
-      tag="main"
-      style={{ width: 240 }}
-      modules={[Navigation]}
-      navigation
-      loop
-      speed={500}
-      spaceBetween={0}
-      slidesPerView={1}
-      slidesPerGroup={1}
-      simulateTouch={false}
-    >
-      <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
-      <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
-      <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
-    </Swiper>
-  </div>
-);
+  return (
+    <div className={classes.block}>
+      <header className={classes.header}>{header}</header>
+      <Swiper
+        tag="main"
+        className={classes.swiper}
+        modules={[Navigation]}
+        loop
+        speed={500}
+        spaceBetween={0}
+        slidesPerView={1}
+        slidesPerGroup={1}
+        simulateTouch={false}
+        navigation={{
+          prevEl: navigationPrevRef.current,
+          nextEl: navigationNextRef.current,
+        }}
+        onInit={(swiper) => {
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
+      >
+        <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
+        <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
+        <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
+        <div className={classes.swiperNavPrev} ref={navigationPrevRef} />
+        <div className={classes.swiperNavNext} ref={navigationNextRef} />
+      </Swiper>
+    </div>
+  );
+};
 
 export default SidebarBlock;
