6bd82c5573ce05c43ecd2e203e88196aa00d66a0
ArtemMayorov <artmai.pg@outlook.com>
2022-08-16 20:48:27 +0300

    SP-34 refactor sidebar add swiper

--- src/components/sidebar-info/SidebarBlock.tsx
+++ src/components/sidebar-info/SidebarBlock.tsx
@@ -1,55 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/modules/navigation/navigation.scss';
+import 'swiper/swiper.scss';
+import { Navigation } from 'swiper';
 import classes from './SidebarInfo.module.scss';
-import left_arrow from '../../assets/icons/left-arrow.svg';
-import right_arrow from '../../assets/icons/right-arrow.svg';
 
 type PropTypes = {
-  header: string,
-  text: string
+  header: string;
+  text: string;
 };
 
-const SidebarBlock: React.FC<PropTypes> = ({ header, text }) => {
-  const [offset, setOffset] = useState(0);
-  const handleLeftClick = () => {
-    setOffset((currentOffset) => {
-      const newOffset = currentOffset + 280;
-
-      return Math.min(newOffset, 0);
-    });
-  };
-  const handleRightClick = () => {
-    setOffset((currentOffset) => {
-      const newOffset = currentOffset - 280;
-
-      return Math.max(newOffset, -560);
-    });
-  };
-
-  return (
-    <div className={classes.block}>
-      <header className={classes.header}>
-        {header}
-      </header>
-      <main className={classes.main}>
-        <input className={classes.svg_l} onClick={handleLeftClick} type="image" src={left_arrow} alt="стрелка" />
-        <div
-          className={classes.al_div_container}
-          style={{ transform: `translateX(${offset}px)` }}
-        >
-          <span className={classes.span}>
-            { text }
-          </span>
-          <span className={classes.span}>
-            { text }
-          </span>
-          <span className={classes.span}>
-            { text }
-          </span>
-        </div>
-        <input className={classes.svg} onClick={handleRightClick} type="image" src={right_arrow} alt="стрелка" />
-      </main>
-    </div>
-  );
-};
+const SidebarBlock: React.FC<PropTypes> = ({ header, text }) => (
+  <div className={classes.block}>
+    <header className={classes.header}>{header}</header>
+    <Swiper
+      tag="main"
+      style={{ width: 240 }}
+      modules={[Navigation]}
+      navigation
+      loop
+      speed={500}
+      spaceBetween={0}
+      slidesPerView={1}
+      slidesPerGroup={1}
+      simulateTouch={false}
+    >
+      <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
+      <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
+      <SwiperSlide className={classes.span} tag="span">{text}</SwiperSlide>
+    </Swiper>
+  </div>
+);
 
 export default SidebarBlock;
