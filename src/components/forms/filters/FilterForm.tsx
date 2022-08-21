fef76f79d83e4aff10244bdb17c2373b09d3a8d3
Grigory Makarov <makkgregory@gmail.com>
2022-08-21 18:27:06 +0300

    implement generic filter form

---
+++ src/components/forms/filters/FilterForm.tsx
@@ -0,0 +1,49 @@
+import { Field, Form, Formik, useFormikContext } from 'formik';
+import { debounce } from 'lodash';
+import React, { useMemo } from "react";
+
+const SubmitOnChangeToken: React.FC = () => {
+  const { values, submitForm } = useFormikContext();
+  const debounceSubmit = useMemo(() => debounce(submitForm, 1000), [submitForm]);
+  React.useEffect(() => {
+    debounceSubmit()
+  }, [values, debounceSubmit]);
+  return null;
+}
+
+export interface FilterFormProps<T> {
+  initialValues: T;
+  onChange: (value: T) => void;
+  children: React.ReactNode;
+}
+
+export default function FilterForm<T>({ initialValues, onChange, children }: FilterFormProps<T>) {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onChange}
+    >
+      {() => (
+        <Form>
+          {children}
+          <SubmitOnChangeToken />
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export interface FilterFormFieldProps {
+  readonly label: string;
+  readonly type: string;
+  readonly name: string;
+}
+
+export const FilterFormField: React.FC<FilterFormFieldProps> = ({ label, type, name }) => {
+  return (
+    <label>
+      {label}
+      <Field type={type} name={name} />
+    </label>
+  )
+}
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Footer from '../footer/Footer';
+import FilterForm, { FilterFormField } from '../forms/filters/FilterForm';
 import PanelList from '../panel-list/PanelList';
-import SidebarPet from '../sidebar-pet/SidebarPet';
 import SidebarInfo from '../sidebar-info/SidebarInfo';
+import SidebarPet from '../sidebar-pet/SidebarPet';
 import classes from './Main.module.scss';
 
 const Main: React.FC = () => (
@@ -10,6 +11,15 @@ const Main: React.FC = () => (
     <SidebarPet />
     <main className={classes.main}>
       <PanelList />
+      <FilterForm initialValues={{ email: "", login: "" }} onChange={(value) => console.log(value)}>
+        <FilterFormField type="text" name="email" label="Email" />
+        <FilterFormField type="text" name="login" label="Login" />
+      </FilterForm>
+
+      <FilterForm initialValues={{ manufacturer: "", isLegal: true }} onChange={(value) => console.log(value)}>
+        <FilterFormField type="text" name="manufacturer" label="Manufacturer" />
+        <FilterFormField type="checkbox" name="isLegal" label="Is legal" />
+      </FilterForm>
       <Footer />
     </main>
     <SidebarInfo />
