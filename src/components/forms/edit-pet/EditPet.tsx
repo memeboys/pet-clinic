9045f3633aba66062519ad701396df16c8bdb55f
memeboys <simonsnkn@gmail.com>
2022-08-23 15:13:53 +0300

    PC-33: fix-problems-with-eslint

--- src/components/forms/edit-pet/EditPet.tsx
+++ src/components/forms/edit-pet/EditPet.tsx
@@ -4,9 +4,11 @@ import React, { useState } from 'react';
 import * as yup from 'yup';
 import classes from './EditPet.module.scss';
 import PetService from '../../../services/PetServices';
-import { PetDTO, PetGender, PetSize, PetType } from '../../../types/PetsDTO';
+import {
+  PetDTO, PetGender, PetSize, PetType,
+} from '../../../types/PetsDTO';
 
-export interface EditPetProps  {
+export interface EditPetProps {
   pet: PetDTO;
 }
 
@@ -40,181 +42,181 @@ const EditPet: React.FC<EditPetProps> = ({ pet }) => {
       <Button type="primary" onClick={showModal}>
         Open Modal
       </Button>
-      <Modal visible={isModalVisible} footer={null} maskClosable={true} onCancel={closeModal}>
+      <Modal visible={isModalVisible} footer={null} maskClosable onCancel={closeModal}>
         <Formik
           initialValues={pet}
           onSubmit={(values) => { onSubmit(values); }}
           validationSchema={validationSchema}
         >
-        {({
-          handleChange, handleBlur, values, isValid, dirty, touched, errors,
-        }) => (
-          <Form>
-            <div>
-              <h4 className={classes.form_title}>Редактировать питомца</h4>
-              <div className={classes.input_wrapper}>
-                <label htmlFor="name">
-                  Имя
-                  <Field
-                    id="name"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
-                    type="text"
-                    name="name"
-                    className={classes.input}
-                  />
-                  {touched.name && errors.name && <span className={classes.input_wrapper__error}>{errors.name}</span>}
-                </label>
-              </div>
-              <div className={classes.input_wrapper}>
-                <label htmlFor="weight">
-                  Вес
-                  <Field
-                    id="weight"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.weight}
-                    name="weight"
-                    className={classes.input}
-                  />
-                  {touched.weight && errors.weight && <span className={classes.input_wrapper__error}>{errors.weight}</span>}
-                </label>
-              </div>
-              <div className={classes.input_wrapper}>
-                <label htmlFor="breed">
-                  Порода
-                  <Field
-                    id="breed"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.breed}
-                    name="breed"
-                    className={classes.input}
-                  />
-                  {touched.breed && errors.breed && <span className={classes.input_wrapper__error}>{errors.breed}</span>}
-                </label>
-              </div>
-              <div className={classes.input_wrapper}>
-                <label htmlFor="color">
-                  Окрас
-                  <Field
-                    id="color"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.color}
-                    name="color"
-                    className={classes.input}
-                  />
-                  {touched.color && errors.color && <span className={classes.input_wrapper__error}>{errors.color}</span>}
-                </label>
-              </div>
-              <div className={classes.input_wrapper}>
-                <label htmlFor="description">
-                  Описание
-                  <Field
-                    id="description"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.description}
-                    name="description"
-                    className={classes.input}
-                  />
-                  {touched.description && errors.description && <span className={classes.input_wrapper__error}>{errors.description}</span>}
-                </label>
-              </div>
-              <div className={classes.input_wrapper}>
-                <label htmlFor="avatar">
-                  Фотография
-                  <Field
-                    id="avatar"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.avatar}
-                    name="avatar"
-                    className={classes.input}
-                  />
-                  {touched.avatar && errors.avatar && <span className={classes.input_wrapper__error}>{errors.avatar}</span>}
-                </label>
-              </div>
-              <div className={classes.input_wrapper}>
-                <label htmlFor="birthDay">
-                  Дата рождения
-                  <Field
-                    id="birthDay"
-                    type="date"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.birthDay}
-                    name="birthDay"
-                    className={classes.input}
-                  />
-                  {touched.birthDay && errors.birthDay && <span className={classes.input_wrapper__error}>{errors.birthDay}</span>}
-                </label>
-              </div>
-              <div className={classes.input_wrapper}>
-              <label htmlFor='petType'>
-                Вид
-                <Field 
-                  id="petType"
-                  as="select"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  name="petType" 
-                  className={classes.input} 
+          {({
+            handleChange, handleBlur, values, isValid, dirty, touched, errors,
+          }) => (
+            <Form>
+              <div>
+                <h4 className={classes.form_title}>Редактировать питомца</h4>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="name">
+                    Имя
+                    <Field
+                      id="name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                      type="text"
+                      name="name"
+                      className={classes.input}
+                    />
+                    {touched.name && errors.name && <span className={classes.input_wrapper__error}>{errors.name}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="weight">
+                    Вес
+                    <Field
+                      id="weight"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.weight}
+                      name="weight"
+                      className={classes.input}
+                    />
+                    {touched.weight && errors.weight && <span className={classes.input_wrapper__error}>{errors.weight}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="breed">
+                    Порода
+                    <Field
+                      id="breed"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.breed}
+                      name="breed"
+                      className={classes.input}
+                    />
+                    {touched.breed && errors.breed && <span className={classes.input_wrapper__error}>{errors.breed}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="color">
+                    Окрас
+                    <Field
+                      id="color"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.color}
+                      name="color"
+                      className={classes.input}
+                    />
+                    {touched.color && errors.color && <span className={classes.input_wrapper__error}>{errors.color}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="description">
+                    Описание
+                    <Field
+                      id="description"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.description}
+                      name="description"
+                      className={classes.input}
+                    />
+                    {touched.description && errors.description && <span className={classes.input_wrapper__error}>{errors.description}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="avatar">
+                    Фотография
+                    <Field
+                      id="avatar"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.avatar}
+                      name="avatar"
+                      className={classes.input}
+                    />
+                    {touched.avatar && errors.avatar && <span className={classes.input_wrapper__error}>{errors.avatar}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="birthDay">
+                    Дата рождения
+                    <Field
+                      id="birthDay"
+                      type="date"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.birthDay}
+                      name="birthDay"
+                      className={classes.input}
+                    />
+                    {touched.birthDay && errors.birthDay && <span className={classes.input_wrapper__error}>{errors.birthDay}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="petType">
+                    Вид
+                    <Field
+                      id="petType"
+                      as="select"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="petType"
+                      className={classes.input}
+                    >
+                      <option value={PetType.CAT}>Cat</option>
+                      <option value={PetType.DOG}>Dog</option>
+                    </Field>
+                    {touched.petType && errors.petType && <span className={classes.input_wrapper__error}>{errors.petType}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="gender">
+                    Пол
+                    <Field
+                      id="gender"
+                      as="select"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="gender"
+                      className={classes.input}
+                    >
+                      <option value={PetGender.MALE}>Мужской</option>
+                      <option value={PetGender.FEMALE}>Женский</option>
+                      <option value={PetGender.OTHER}>Другой</option>
+                    </Field>
+                    {touched.gender && errors.gender && <span className={classes.input_wrapper__error}>{errors.gender}</span>}
+                  </label>
+                </div>
+                <div className={classes.input_wrapper}>
+                  <label htmlFor="size">
+                    Размер
+                    <Field
+                      id="size"
+                      as="select"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="size"
+                      className={classes.input}
+                    >
+                      <option value={PetSize.SMALL}>SMALL</option>
+                    </Field>
+                    {touched.gender && errors.gender && <span className={classes.input_wrapper__error}>{errors.gender}</span>}
+                  </label>
+                </div>
+                <button
+                  disabled={!isValid && !dirty}
+                  type="submit"
                 >
-                  <option value={PetType.CAT}>Cat</option>
-                  <option value={PetType.DOG}>Dog</option>
-                </Field>
-                {touched.petType && errors.petType && <span className={classes.input_wrapper__error}>{errors.petType}</span>}
-              </label>
-            </div>
-            <div className={classes.input_wrapper}>
-              <label htmlFor='gender'>
-                Пол
-                <Field 
-                  id="gender"
-                  as="select"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  name="gender" 
-                  className={classes.input} 
-                >
-                  <option value={PetGender.MALE}>Мужской</option>
-                  <option value={PetGender.FEMALE}>Женский</option>
-                  <option value={PetGender.OTHER}>Другой</option>
-                </Field>
-                {touched.gender && errors.gender && <span className={classes.input_wrapper__error}>{errors.gender}</span>}
-              </label>
-            </div>
-            <div className={classes.input_wrapper}>
-              <label htmlFor='size'>
-                Размер
-                <Field 
-                  id="size"
-                  as="select"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  name="size" 
-                  className={classes.input} 
-                >
-                  <option value={PetSize.SMALL}>SMALL</option>
-                </Field>
-                {touched.gender && errors.gender && <span className={classes.input_wrapper__error}>{errors.gender}</span>}
-              </label>
-            </div>
-              <button
-                disabled={!isValid && !dirty}
-                type="submit"
-              >
-                Submit
-              </button>
-            </div>
-          </Form>
-        )}
+                  Submit
+                </button>
+              </div>
+            </Form>
+          )}
         </Formik>
       </Modal>
-  </div>
+    </div>
   );
-}
+};
 export default EditPet;
