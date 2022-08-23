0c487ff460e1d8232bbf19b209516b0cb10141b4
memeboys <simonsnkn@gmail.com>
2022-08-23 14:53:36 +0300

    PC-23: add-filter-method

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -1,64 +1,49 @@
-import React from 'react';
-import { FilterConfig, FilterKind } from './FiltersData';
+import { Field, Form, Formik, useFormikContext } from 'formik';
+import { debounce } from 'lodash';
+import React, { useMemo } from 'react';
 
-interface SearchFilterViewProps {
+const SubmitOnChangeToken: React.FC = () => {
+  const { values, submitForm } = useFormikContext();
+  const debounceSubmit = useMemo(() => debounce(submitForm, 1000), [submitForm]);
+  React.useEffect(() => {
+    debounceSubmit()
+  }, [values, debounceSubmit]);
+  return null;
+}
+
+export interface FilterFormProps<T> {
+  initialValues: T;
+  onChange: (value: T) => void;
+  children: React.ReactNode;
+}
+
+export default function FilterForm<T>({ initialValues, onChange, children }: FilterFormProps<T>) {
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onChange}
+    >
+      {() => (
+        <Form>
+          {children}
+          <SubmitOnChangeToken />
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export interface FilterFormFieldProps {
+  readonly label: string;
+  readonly type: string;
   readonly name: string;
 }
 
-const SearchFilterView: React.FC<SearchFilterViewProps> = ({ name }) => (
-  <div className="search-filter">
-    <span>{name}</span>
-    <input type="search" />
-  </div>
-);
-
-interface TextFilterViewProps {
-  readonly name: string;
+export const FilterFormField: React.FC<FilterFormFieldProps> = ({ label, type, name }) => {
+  return (
+    <label>
+      {label}
+      <Field type={type} name={name} />
+    </label>
+  )
 }
-
-const TextFilterView: React.FC<TextFilterViewProps> = ({ name }) => (
-  <div className="text-filter">
-    <span>{name}</span>
-    <input type="text" />
-  </div>
-);
-
-interface BoolFilterViewProps {
-  readonly name: string;
-}
-
-const BoolFilterView: React.FC<BoolFilterViewProps> = ({ name }) => (
-  <div className="bool-filter">
-    <span>{name}</span>
-    <input type="checkbox" />
-  </div>
-);
-
-interface FilterViewProps {
-  readonly filter: FilterConfig;
-}
-
-const FilterView: React.FC<FilterViewProps> = ({ filter }) => {
-  switch (filter.kind) {
-    case FilterKind.SEARCH:
-      return <SearchFilterView name={filter.name} />;
-    case FilterKind.TEXT:
-      return <TextFilterView name={filter.name} />;
-    case FilterKind.BOOL:
-      return <BoolFilterView name={filter.name} />;
-    default:
-      throw new Error('Unknown filter kind');
-  }
-};
-
-export interface FiltersProps {
-  readonly filters: readonly FilterConfig[];
-}
-
-export const Filters: React.FC<FiltersProps> = ({ filters }) => (
-  <div className="filters">
-    {filters.map((filter) => (
-      <FilterView key={filter.name} filter={filter} />
-    ))}
-  </div>
-);
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,12 +4,22 @@ import PanelList from '../panel-list/PanelList';
 import SidebarPet from '../sidebar-pet/SidebarPet';
 import SidebarInfo from '../sidebar-info/SidebarInfo';
 import classes from './Main.module.scss';
+import FilterForm, { FilterFormField } from '../filters/Filters';
 
 const Main: React.FC = () => (
   <div className={classes.main_window}>
     <SidebarPet />
     <main className={classes.main}>
       <PanelList />
+      <FilterForm initialValues={{ email: "", login: "" }} onChange={(value) => console.log(value)}>
+        <FilterFormField type="text" name="email" label="Email" />
+        <FilterFormField type="text" name="login" label="Login" />
+      </FilterForm>
+
+      <FilterForm initialValues={{ manufacturer: "", isLegal: true }} onChange={(value) => console.log(value)}>
+        <FilterFormField type="text" name="manufacturer" label="Manufacturer" />
+        <FilterFormField type="checkbox" name="isLegal" label="Is legal" />
+      </FilterForm>
       <Footer />
     </main>
     <SidebarInfo />
